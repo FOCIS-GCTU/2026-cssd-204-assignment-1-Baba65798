00cf14253d86c20cbcb618245fb87687013ecb84
Baba65798 <2425404039@live.gctu.edu.gh>
2026-05-31 03:40:33 +0000

    Add files via upload

---
+++ NBA.py
@@ -0,0 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 2
+#
+# Name: Nicholas Baba
+# STUDENT ID:  2425404039
+# Email: babanich65@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Nicholas Baba, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials NBA (A used as stand-in for missing middle initial)
+    # in both small and large letter format using only print and strings
+
+    print()
+    print("...NBA")
+    print()
+    print("...NN........NN.......BBBBBBBBB..............AAAA........")
+    print("...NNN.......NN.......BB......BBB...........AA.AA........")
+    print("...NNNN......NN.......BB.......BB..........AA..AA........")
+    print("...NN.NN.....NN.......BB......BBB.........AA...AA........")
+    print("...NN..NN....NN.......BBBBBBBBB..........AAAAAAAAAA......")
+    print("...NN...NN...NN.......BB......BBB........AA......AA......")
+    print("...NN....NN..NN.......BB.......BB........AA......AA......")
+    print("...NN.....NNN.N.......BB.......BB........AA......AA......")
+    print("...NN......NNNN****...BB......BBB.****...AA......AA..****")
+    print("...NN.......NNN****...BBBBBBBBB...****...AA......AA..****")
+    print()
+
+
+main()
